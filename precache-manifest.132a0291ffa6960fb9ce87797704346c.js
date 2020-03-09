88f9d565576640f3c4939b311d622b3962ed687f
danielmattson <daniel18mattson@gmail.com>
2020-03-08 22:21:28 -0400

    Updates

---
+++ precache-manifest.132a0291ffa6960fb9ce87797704346c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2d9ee3bd49da954675e7b84ca080952",
+    "url": "/ChowNow/index.html"
+  },
+  {
+    "revision": "aad672731132876597b2",
+    "url": "/ChowNow/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3a8866e6f6bf196a254f",
+    "url": "/ChowNow/static/js/2.01fd1c34.chunk.js"
+  },
+  {
+    "revision": "410d200f0968c8747afbfc6cb50c985b",
+    "url": "/ChowNow/static/js/2.01fd1c34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aad672731132876597b2",
+    "url": "/ChowNow/static/js/main.bf1cbc47.chunk.js"
+  },
+  {
+    "revision": "9e22ff1a644393aa5b1d",
+    "url": "/ChowNow/static/js/runtime-main.d4fedcc8.js"
+  }
+]);
